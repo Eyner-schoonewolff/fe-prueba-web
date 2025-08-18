cabc32ea5ab755ec830ac84f6baf4bb89025a448
Eyner-schoonewolff <enewolff2013@gmail.com>
2025-08-18 16:21:01 -0500

    feat(core): se agrega configuracion para img obtenidas desde s3 publico

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "public-assets.wompi.com"],
+    domains: ["images.unsplash.com", "public-assets.wompi.com", "productos-wompi.s3.us-west-2.amazonaws.com"],
   },
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
